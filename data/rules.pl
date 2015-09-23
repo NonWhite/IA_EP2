102bd37d8bfa88e5d0245639e3de0062f3586a69
NonWhite <wperezurcia@gmail.com>
2015-09-23 13:40:19 -0300

    version 0.1, not very descriptive rules

---
+++ data/rules.pl
@@ -0,0 +1,303 @@
+guess( bulbasaur ) :- bulbasaur , !.
+guess( ivysaur ) :- ivysaur , !.
+guess( venusaur ) :- venusaur , !.
+guess( charmander ) :- charmander , !.
+guess( charmeleon ) :- charmeleon , !.
+guess( charizard ) :- charizard , !.
+guess( squirtle ) :- squirtle , !.
+guess( wartortle ) :- wartortle , !.
+guess( blastoise ) :- blastoise , !.
+guess( caterpie ) :- caterpie , !.
+guess( metapod ) :- metapod , !.
+guess( butterfree ) :- butterfree , !.
+guess( weedle ) :- weedle , !.
+guess( kakuna ) :- kakuna , !.
+guess( beedrill ) :- beedrill , !.
+guess( pidgey ) :- pidgey , !.
+guess( pidgeotto ) :- pidgeotto , !.
+guess( pidgeot ) :- pidgeot , !.
+guess( rattata ) :- rattata , !.
+guess( raticate ) :- raticate , !.
+guess( spearow ) :- spearow , !.
+guess( fearow ) :- fearow , !.
+guess( ekans ) :- ekans , !.
+guess( arbok ) :- arbok , !.
+guess( pikachu ) :- pikachu , !.
+guess( raichu ) :- raichu , !.
+guess( sandshrew ) :- sandshrew , !.
+guess( sandslash ) :- sandslash , !.
+guess( nidoran_female ) :- nidoran_female , !.
+guess( nidorina ) :- nidorina , !.
+guess( nidoqueen ) :- nidoqueen , !.
+guess( nidoran_male ) :- nidoran_male , !.
+guess( nidorino ) :- nidorino , !.
+guess( nidoking ) :- nidoking , !.
+guess( clefairy ) :- clefairy , !.
+guess( clefable ) :- clefable , !.
+guess( vulpix ) :- vulpix , !.
+guess( ninetales ) :- ninetales , !.
+guess( jigglypuff ) :- jigglypuff , !.
+guess( wigglytuff ) :- wigglytuff , !.
+guess( zubat ) :- zubat , !.
+guess( golbat ) :- golbat , !.
+guess( oddish ) :- oddish , !.
+guess( gloom ) :- gloom , !.
+guess( vileplume ) :- vileplume , !.
+guess( paras ) :- paras , !.
+guess( parasect ) :- parasect , !.
+guess( venonat ) :- venonat , !.
+guess( venomoth ) :- venomoth , !.
+guess( diglett ) :- diglett , !.
+guess( dugtrio ) :- dugtrio , !.
+guess( meowth ) :- meowth , !.
+guess( persian ) :- persian , !.
+guess( psyduck ) :- psyduck , !.
+guess( golduck ) :- golduck , !.
+guess( mankey ) :- mankey , !.
+guess( primeape ) :- primeape , !.
+guess( growlithe ) :- growlithe , !.
+guess( arcanine ) :- arcanine , !.
+guess( poliwag ) :- poliwag , !.
+guess( poliwhirl ) :- poliwhirl , !.
+guess( poliwrath ) :- poliwrath , !.
+guess( abra ) :- abra , !.
+guess( kadabra ) :- kadabra , !.
+guess( alakazam ) :- alakazam , !.
+guess( machop ) :- machop , !.
+guess( machoke ) :- machoke , !.
+guess( machamp ) :- machamp , !.
+guess( bellsprout ) :- bellsprout , !.
+guess( weepinbell ) :- weepinbell , !.
+guess( victreebel ) :- victreebel , !.
+guess( tentacool ) :- tentacool , !.
+guess( tentacruel ) :- tentacruel , !.
+guess( geodude ) :- geodude , !.
+guess( graveler ) :- graveler , !.
+guess( golem ) :- golem , !.
+guess( ponyta ) :- ponyta , !.
+guess( rapidash ) :- rapidash , !.
+guess( slowpoke ) :- slowpoke , !.
+guess( slowbro ) :- slowbro , !.
+guess( magnemite ) :- magnemite , !.
+guess( magneton ) :- magneton , !.
+guess( farfetchd ) :- farfetchd , !.
+guess( doduo ) :- doduo , !.
+guess( dodrio ) :- dodrio , !.
+guess( seel ) :- seel , !.
+guess( dewgong ) :- dewgong , !.
+guess( grimer ) :- grimer , !.
+guess( muk ) :- muk , !.
+guess( shellder ) :- shellder , !.
+guess( cloyster ) :- cloyster , !.
+guess( gastly ) :- gastly , !.
+guess( haunter ) :- haunter , !.
+guess( gengar ) :- gengar , !.
+guess( onix ) :- onix , !.
+guess( drowzee ) :- drowzee , !.
+guess( hypno ) :- hypno , !.
+guess( krabby ) :- krabby , !.
+guess( kingler ) :- kingler , !.
+guess( voltorb ) :- voltorb , !.
+guess( electrode ) :- electrode , !.
+guess( exeggcute ) :- exeggcute , !.
+guess( exeggutor ) :- exeggutor , !.
+guess( cubone ) :- cubone , !.
+guess( marowak ) :- marowak , !.
+guess( hitmonlee ) :- hitmonlee , !.
+guess( hitmonchan ) :- hitmonchan , !.
+guess( lickitung ) :- lickitung , !.
+guess( koffing ) :- koffing , !.
+guess( weezing ) :- weezing , !.
+guess( rhyhorn ) :- rhyhorn , !.
+guess( rhydon ) :- rhydon , !.
+guess( chansey ) :- chansey , !.
+guess( tangela ) :- tangela , !.
+guess( kangaskhan ) :- kangaskhan , !.
+guess( horsea ) :- horsea , !.
+guess( seadra ) :- seadra , !.
+guess( goldeen ) :- goldeen , !.
+guess( seaking ) :- seaking , !.
+guess( staryu ) :- staryu , !.
+guess( starmie ) :- starmie , !.
+guess( mr_mime ) :- mr_mime , !.
+guess( scyther ) :- scyther , !.
+guess( jynx ) :- jynx , !.
+guess( electabuzz ) :- electabuzz , !.
+guess( magmar ) :- magmar , !.
+guess( pinsir ) :- pinsir , !.
+guess( tauros ) :- tauros , !.
+guess( magikarp ) :- magikarp , !.
+guess( gyarados ) :- gyarados , !.
+guess( lapras ) :- lapras , !.
+guess( ditto ) :- ditto , !.
+guess( eevee ) :- eevee , !.
+guess( vaporeon ) :- vaporeon , !.
+guess( jolteon ) :- jolteon , !.
+guess( flareon ) :- flareon , !.
+guess( porygon ) :- porygon , !.
+guess( omanyte ) :- omanyte , !.
+guess( omastar ) :- omastar , !.
+guess( kabuto ) :- kabuto , !.
+guess( kabutops ) :- kabutops , !.
+guess( aerodactyl ) :- aerodactyl , !.
+guess( snorlax ) :- snorlax , !.
+guess( articuno ) :- articuno , !.
+guess( zapdos ) :- zapdos , !.
+guess( moltres ) :- moltres , !.
+guess( dratini ) :- dratini , !.
+guess( dragonair ) :- dragonair , !.
+guess( dragonite ) :- dragonite , !.
+guess( mewtwo ) :- mewtwo , !.
+guess( mew ) :- mew , !.
+
+bulbasaur :- verify( grass ) , verify( poison ) , !.
+ivysaur :- verify( grass ) , verify( poison ) , !.
+venusaur :- verify( grass ) , verify( poison ) , !.
+charmander :- verify( fire ) , !.
+charmeleon :- verify( fire ) , !.
+charizard :- verify( fire ) , verify( flying ) , verify( dragon ) , !.
+squirtle :- verify( water ) , !.
+wartortle :- verify( water ) , !.
+blastoise :- verify( water ) , !.
+caterpie :- verify( bug ) , !.
+metapod :- verify( bug ) , !.
+butterfree :- verify( flying ) , verify( bug ) , !.
+weedle :- verify( bug ) , verify( poison ) , !.
+kakuna :- verify( bug ) , verify( poison ) , !.
+beedrill :- verify( bug ) , verify( poison ) , !.
+pidgey :- verify( flying ) , verify( normal ) , !.
+pidgeotto :- verify( flying ) , verify( normal ) , !.
+pidgeot :- verify( flying ) , verify( normal ) , !.
+rattata :- verify( normal ) , !.
+raticate :- verify( normal ) , !.
+spearow :- verify( flying ) , verify( normal ) , !.
+fearow :- verify( flying ) , verify( normal ) , !.
+ekans :- verify( poison ) , !.
+arbok :- verify( poison ) , !.
+pikachu :- verify( electric ) , !.
+raichu :- verify( electric ) , !.
+sandshrew :- verify( ground ) , !.
+sandslash :- verify( ground ) , !.
+nidoran_female :- verify( poison ) , !.
+nidorina :- verify( poison ) , !.
+nidoqueen :- verify( poison ) , verify( ground ) , !.
+nidoran_male :- verify( poison ) , !.
+nidorino :- verify( poison ) , !.
+nidoking :- verify( poison ) , verify( ground ) , !.
+clefairy :- verify( fairy ) , !.
+clefable :- verify( fairy ) , !.
+vulpix :- verify( fire ) , !.
+ninetales :- verify( fire ) , !.
+jigglypuff :- verify( fairy ) , verify( normal ) , !.
+wigglytuff :- verify( fairy ) , verify( normal ) , !.
+zubat :- verify( flying ) , verify( poison ) , !.
+golbat :- verify( flying ) , verify( poison ) , !.
+oddish :- verify( grass ) , verify( poison ) , !.
+gloom :- verify( grass ) , verify( poison ) , !.
+vileplume :- verify( grass ) , verify( poison ) , !.
+paras :- verify( grass ) , verify( bug ) , !.
+parasect :- verify( grass ) , verify( bug ) , !.
+venonat :- verify( bug ) , verify( poison ) , !.
+venomoth :- verify( bug ) , verify( poison ) , !.
+diglett :- verify( ground ) , !.
+dugtrio :- verify( ground ) , !.
+meowth :- verify( normal ) , !.
+persian :- verify( normal ) , !.
+psyduck :- verify( water ) , !.
+golduck :- verify( water ) , !.
+mankey :- verify( fighting ) , !.
+primeape :- verify( fighting ) , !.
+growlithe :- verify( fire ) , !.
+arcanine :- verify( fire ) , !.
+poliwag :- verify( water ) , !.
+poliwhirl :- verify( water ) , !.
+poliwrath :- verify( water ) , verify( fighting ) , !.
+abra :- verify( psychic ) , !.
+kadabra :- verify( psychic ) , !.
+alakazam :- verify( psychic ) , !.
+machop :- verify( fighting ) , !.
+machoke :- verify( fighting ) , !.
+machamp :- verify( fighting ) , !.
+bellsprout :- verify( grass ) , verify( poison ) , !.
+weepinbell :- verify( grass ) , verify( poison ) , !.
+victreebel :- verify( grass ) , verify( poison ) , !.
+tentacool :- verify( water ) , verify( poison ) , !.
+tentacruel :- verify( water ) , verify( poison ) , !.
+geodude :- verify( ground ) , verify( rock ) , !.
+graveler :- verify( ground ) , verify( rock ) , !.
+golem :- verify( ground ) , verify( rock ) , !.
+ponyta :- verify( fire ) , !.
+rapidash :- verify( fire ) , !.
+slowpoke :- verify( water ) , verify( psychic ) , !.
+slowbro :- verify( water ) , verify( psychic ) , !.
+magnemite :- verify( steel ) , verify( electric ) , !.
+magneton :- verify( steel ) , verify( electric ) , !.
+farfetchd :- verify( flying ) , verify( normal ) , !.
+doduo :- verify( flying ) , verify( normal ) , !.
+dodrio :- verify( flying ) , verify( normal ) , !.
+seel :- verify( water ) , !.
+dewgong :- verify( water ) , verify( ice ) , !.
+grimer :- verify( poison ) , !.
+muk :- verify( poison ) , !.
+shellder :- verify( water ) , !.
+cloyster :- verify( water ) , verify( ice ) , !.
+gastly :- verify( ghost ) , verify( poison ) , !.
+haunter :- verify( ghost ) , verify( poison ) , !.
+gengar :- verify( ghost ) , verify( poison ) , !.
+onix :- verify( ground ) , verify( rock ) , !.
+drowzee :- verify( psychic ) , !.
+hypno :- verify( psychic ) , !.
+krabby :- verify( water ) , !.
+kingler :- verify( water ) , !.
+voltorb :- verify( electric ) , !.
+electrode :- verify( electric ) , !.
+exeggcute :- verify( psychic ) , verify( grass ) , !.
+exeggutor :- verify( psychic ) , verify( grass ) , !.
+cubone :- verify( ground ) , !.
+marowak :- verify( ground ) , !.
+hitmonlee :- verify( fighting ) , !.
+hitmonchan :- verify( fighting ) , !.
+lickitung :- verify( normal ) , !.
+koffing :- verify( poison ) , !.
+weezing :- verify( poison ) , !.
+rhyhorn :- verify( rock ) , verify( ground ) , !.
+rhydon :- verify( rock ) , verify( ground ) , !.
+chansey :- verify( normal ) , !.
+tangela :- verify( grass ) , !.
+kangaskhan :- verify( normal ) , !.
+horsea :- verify( water ) , !.
+seadra :- verify( water ) , !.
+goldeen :- verify( water ) , !.
+seaking :- verify( water ) , !.
+staryu :- verify( water ) , !.
+starmie :- verify( water ) , verify( psychic ) , !.
+mr_mime :- verify( psychic ) , verify( fairy ) , !.
+scyther :- verify( flying ) , verify( bug ) , !.
+jynx :- verify( psychic ) , verify( ice ) , !.
+electabuzz :- verify( electric ) , !.
+magmar :- verify( fire ) , !.
+pinsir :- verify( flying ) , verify( bug ) , !.
+tauros :- verify( normal ) , !.
+magikarp :- verify( water ) , !.
+gyarados :- verify( water ) , verify( dark ) , verify( flying ) , !.
+lapras :- verify( water ) , verify( ice ) , !.
+ditto :- verify( normal ) , !.
+eevee :- verify( normal ) , !.
+vaporeon :- verify( water ) , !.
+jolteon :- verify( electric ) , !.
+flareon :- verify( fire ) , !.
+porygon :- verify( normal ) , !.
+omanyte :- verify( water ) , verify( rock ) , !.
+omastar :- verify( water ) , verify( rock ) , !.
+kabuto :- verify( water ) , verify( rock ) , !.
+kabutops :- verify( water ) , verify( rock ) , !.
+aerodactyl :- verify( flying ) , verify( rock ) , !.
+snorlax :- verify( normal ) , !.
+articuno :- verify( flying ) , verify( ice ) , !.
+zapdos :- verify( flying ) , verify( electric ) , !.
+moltres :- verify( fire ) , verify( flying ) , !.
+dratini :- verify( dragon ) , !.
+dragonair :- verify( dragon ) , !.
+dragonite :- verify( flying ) , verify( dragon ) , !.
+mewtwo :- verify( psychic ) , verify( fighting ) , !.
+mew :- verify( psychic ) , !.
